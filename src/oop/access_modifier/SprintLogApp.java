14a38449ded340d256732e07c12169c870b1bddb
LeeKM321 <stephen4951@gmail.com>
2026-05-07 19:03:36 +0900

    Java 객체지향 (접근 제한자) - LearningLog 필드에 private 접근 제한 설정 - 정보 은닉과 캡슐화를 위한 setter 메서드 작성 중

---
+++ src/oop/access_modifier/SprintLogApp.java
@@ -0,0 +1,21 @@
+package oop.access_modifier;
+
+public class SprintLogApp {
+
+    public static void main(String[] args) {
+
+        LearningLog javaLog = new LearningLog("Java 시작", 40);
+        LearningLog gitLog = new LearningLog("Git 복습", 30, false);
+
+//        javaLog.title = "야호";
+//        javaLog.minutes = -374872;
+//        javaLog.publicLog = false; 모든 필드가 private 접근 제한자를 지정했기 때문에 값을 참조하거나 수정이 불가능해 집니다.
+
+        javaLog.minutes = 100;
+
+        javaLog.printSummary();
+
+
+    }
+
+}
