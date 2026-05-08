c1c471e298392f5eacc6601361bf7b49a1a0abfd
LeeKM321 <stephen4951@gmail.com>
2026-05-08 10:12:53 +0900

    Java 객체지향 (정보 은닉과 캡슐화) - 정보 은닉과 캡슐화를 위한 setter 메서드 작성 - 값을 돌려주기 위한 getter 메서드 작성 - title 유효성 검증을 위한 private 메서드 선언 - 3항 연산식 소개

--- src/oop/access_modifier/SprintLogApp.java
+++ src/oop/access_modifier/SprintLogApp.java
@@ -11,10 +11,15 @@ public class SprintLogApp {
 //        javaLog.minutes = -374872;
 //        javaLog.publicLog = false; 모든 필드가 private 접근 제한자를 지정했기 때문에 값을 참조하거나 수정이 불가능해 집니다.
 
-        javaLog.setMinutes(200);
+        javaLog.extendStudy(200);
+        javaLog.changeTitle("Java 객체지향과 캡슐화");
+        javaLog.hideFromPublic();
 
+        gitLog.extendStudy(45);
+        gitLog.openToPublic();
 
         javaLog.printSummary();
+        gitLog.printSummary();
 
 
     }
