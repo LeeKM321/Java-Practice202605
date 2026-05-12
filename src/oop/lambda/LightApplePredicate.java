e4225273f5f78cb6712d523265d0f5d82ce08f6e
LeeKM321 <stephen4951@gmail.com>
2026-05-12 19:03:50 +0900

    Java 함수형 인터페이스와 람다 - 동작의 파라미터화를 위한 인터페이스와 람다식 선언하기 - FilterApple 클래스에 각 problem 및 solution 있습니다!

---
+++ src/oop/lambda/LightApplePredicate.java
@@ -0,0 +1,8 @@
+package oop.lambda;
+
+public class LightApplePredicate implements ApplePredicate {
+    @Override
+    public boolean test(Apple apple) {
+        return apple.getWeight() <= 100;
+    }
+}
